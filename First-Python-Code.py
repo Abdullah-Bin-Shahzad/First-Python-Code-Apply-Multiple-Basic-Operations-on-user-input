b87742574151912314a245c7d430b0afe1b27667
Abdullahsh <abdullahshahzad03@gmail.com>
2023-07-09 02:35:21 +0500

    Add files via upload

---
+++ First-Python-Code.py
@@ -0,0 +1,20 @@
+#Input values from user
+value1=int(input("Enter First Number: "))
+value2=int(input("Enter Second Number: "))
+#Addtion Operator
+Sum= value1+value2
+#Subtraction Operator
+Subtract= value1-value2
+#Addtion Operator
+Mutiply= value1*value2
+#Divide Operator
+Divide= value1/value2
+#Exponential Operator
+Expo= value1**2
+#Module Operator
+Module= value1%value2
+#Floor Division Operator
+FloorDivison= value1//value2
+
+
+print("The Sum of", value1,value2 ,"are:", Sum,"\nThe Subtraction of", value1,value2 ,"are:", Subtract,"\nThe Mutiply of", value1,value2 ,"are:",Mutiply,"\nThe Divide of", value1,value2 ,"are:", Divide,"\nThe Exponential of", value1,"are:", Expo,"\nThe Module of", value1,value2 ,"are:", Module,"\nThe Floor Divison of", value1,value2 ,"are:", FloorDivison)
